689757a9c587508cb231af1a785a8dab1721bde7
OKali <o.kalinowski27@gmail.com>
2020-01-08 15:52:04 -0500

    if variable is even or odd else

--- JavaScript-Practice/loggingLetters.js
+++ JavaScript-Practice/loggingLetters.js
@@ -5,3 +5,18 @@ for (var i=0; i < sentence.length; i++) {
         console.log(sentence[i]);
     }
 }
+
+
+// if (number % 2 == 0) numberIsEven
+// if (number % 2 != 0) numberIsOdd
+
+var n = 25;
+var result = 0;
+
+
+if (n % 2 === 0) {
+    result = result + 10
+}
+else {
+    result = result -n
+}
