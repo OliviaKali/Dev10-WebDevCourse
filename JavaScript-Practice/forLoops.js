bd0b2cae7a634a360967787f4b1f76eb272a668c
OKali <o.kalinowski27@gmail.com>
2020-01-09 12:35:59 -0500

    returning function

--- JavaScript-Practice/forLoops.js
+++ JavaScript-Practice/forLoops.js
@@ -1,3 +1,7 @@
+//This equation adds every postive integer
+// up to 25
+//Example: 1 + 2 + 3 + 4 ... + 25
+
 var sum = 0
 
 for (var n = 25; n > 0; n--) {
---
+++ JavaScript-Practice/returningFunctions.js
@@ -0,0 +1,21 @@
+function addTwoNumbers (firstNumber, secondNumber) {
+    var sum = firstNumber + secondNumber;
+    return sum;
+}
+
+// addTwoNumbers(2,3)
+//console.log(addTwoNumbers(2,3))
+
+
+
+// var sum = function(number1, number2) {
+//     console.log(number1 + number2);
+// };
+
+// result = sum(1,3);
+
+// var sum = function(number1, number2) {
+//     return (number1 + number2);     
+// };
+
+// result = sum(1,3);
